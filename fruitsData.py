e8741036dcad0d6bdbdd2fe5bc9b84a16e9659ff
dmwellsSDBD <55493178+dmwellsSDBD@users.noreply.github.com>
2025-03-05 12:50:42 +0000

    assignment Setup

---
+++ fruitsData.py
@@ -0,0 +1,26 @@
+def main():
+    # Initialize an empty dictionary to store fruit data
+    
+
+    # Get the number of fruits to input (input statement). This will be the 
+    # number of sets of data you need to collect.
+    
+    
+
+    # Collect data for the number of fruits above from the user. Should include name, color, 
+    # weight in lbs, and price. Once each set of data points is collected
+    # Think about what kind of control structure to create to complete this process
+    
+
+        # After each set of input statements, store the data in the dictionary
+        
+
+    
+
+    # Output each of the fruit's data as a vertical list. This happens one time
+    # for each of the fruits in the dictionary. 
+    
+
+if __name__ == "__main__":
+    main()
+
